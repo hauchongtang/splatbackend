a1301da5b24596231d235a03928848493e048e87
hauchongtang <56034480+hauchongtang@users.noreply.github.com>
2022-05-16 23:00:06 +0800

    added gin cors policy

--- main.go
+++ main.go
@@ -3,11 +3,31 @@ package main
 import (
 	"os"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/hauchongtang/splatbackend/middleware"
 	"github.com/hauchongtang/splatbackend/routes"
 )
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "*")
+		/*
+		   c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		   c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		   c.Writer.Header().Set("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
+		   c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS, PATCH")
+		*/
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -16,9 +36,9 @@ func main() {
 		port = "8000"
 	}
 
-	router := gin.New()
+	router := gin.Default()
+	router.Use(CORSMiddleware())
 	router.Use(gin.Logger())
-	router.Use(cors.Default())
 	routes.AuthRoutes(router)
 	routes.UserRoutes(router)
 
